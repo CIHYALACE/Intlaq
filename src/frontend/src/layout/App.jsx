88a2bb170cc445a9e00d248330a27c190d3f68b0
AbdelRahman <abodyoussef2015@gmail.com>
2025-07-13 15:43:17 +0300

    SetUp The Routs in front end

--- src/frontend/src/layout/App.jsx
+++ src/frontend/src/layout/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import '../style/App.css'
+import { Routes, Route } from 'react-router-dom'
+import SharedLayout from '../shared/sharedLayout'
+import HomePage from '../pages/homePage'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <Routes >
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    </Routes>
   )
 }
-
-export default App
--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -3,9 +3,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './layout/App.jsx'
+import { BrowserRouter  } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter >
     <App />
-  </StrictMode>,
+  </BrowserRouter >,
 )
